6957ef03230b52758ecca7ce98bda34c683c1f84
devleopar <oktarkara@gmail.com>
2020-09-14 22:32:37 +0300

    gradient effect added

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 This is a ReactJS based personal resume adaptated by me
 
 ## It is Live
-You can check it on [here]: https://elastic-bassi-8c68e2.netlify.app/
+You can check it on [HERE](https://elastic-bassi-8c68e2.netlify.app/)
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,7 +34,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">I'm <span className="gradient-back">{name}</span>. </h1>
             <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
